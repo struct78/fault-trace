13fda526bd9a194f9149a1cbda79f0b8c68adf0b
struct78 <david@struct.com.au>
2017-01-25 17:20:38 +1100

    Added bash script to edit screen captures
    ./edit-render.sh {movie.mov} {start} {end}

---
+++ src/bash/edit-render.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+if [ "$#" -ne 3 ]; then
+	echo "Requires 3 arguments:"
+	echo "1) Movie file"
+	echo "2) Start time"
+	echo "3) End time"
+	exit
+fi
+
+fn=$1
+
+echo "Converting $fn"
+exec ffmpeg -i "$fn" -vcodec libx264 -acodec aac -strict experimental -y "${fn/.mov/}.mp4" &
+wait
+echo "Cutting movie..."
+exec ffmpeg -i "${fn/.mov/}.mp4" -acodec aac -strict experimental -ss $2 -to $3 -async 1 -y "${fn/.mov/}-ffmpeg.mp4" &
+wait
+echo "Done"
