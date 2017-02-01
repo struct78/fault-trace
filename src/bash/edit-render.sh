79929c1d74d0bca59f467ecb7221c2e95cff5743
struct78 <david@struct.com.au>
2017-02-02 08:49:52 +1100

    Added fade to grey

--- src/bash/edit-render.sh
+++ src/bash/edit-render.sh
@@ -1,18 +1,20 @@
 ﻿#!/bin/bash
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 5 ]; then
 	echo "Requires 3 arguments:"
 	echo "1) Movie file"
 	echo "2) Start time"
 	echo "3) End time"
+	echo "4) Fade out time"
+	echo "5) Fade out duration"
 	exit
 fi
 
 fn=$1
+start=$2
+end=$3
+fadeout=$4
+fadeduration=$5
 
 echo "Converting $fn"
-exec ffmpeg -i "$fn" -vcodec libx264 -acodec aac -strict experimental -y "${fn/.mov/}.mp4" &
-wait
-echo "Cutting movie..."
-exec ffmpeg -i "${fn/.mov/}.mp4" -acodec aac -strict experimental -ss $2 -to $3 -async 1 -y "${fn/.mov/}-ffmpeg.mp4" &
-wait
+exec ffmpeg -i "${fn/.mov/}.mov" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#E1E1E6" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn/.mov/}-ffmpeg.mp4" &
 echo "Done"
