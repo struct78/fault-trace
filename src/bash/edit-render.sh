4ce6f61e725c23d8abd0fb7d330fb6c93004a4ee
struct78 <david@struct.com.au>
2017-10-13 08:35:25 +1100

    Updated bash script

--- src/bash/edit-render.sh
+++ src/bash/edit-render.sh
@@ -7,8 +7,8 @@ if [ "$#" -ne 6 ]; then
 	echo "1) Movie file"
 	echo "2) Start time"
 	echo "3) End time"
-	echo "4) Fade out time"
-	echo "5) Fade out duration"
+	echo "4) Fade out time (in seconds)"
+	echo "5) Fade out duration (Fade out time - this number)"
 	echo "6) Type: 4K (Default), HD, Square"
 	exit
 fi
@@ -24,11 +24,11 @@ echo "Converting $fn"
 echo $type
 
 if [ "$type" == "HD" ]; then
-	exec ffmpeg -i "${fn}" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#090A24,crop=3840:2160:0:120,scale=1920:1080" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn%.*}-hd.mp4" &
+	exec ffmpeg -i "${fn}" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#020C14,crop=3840:2160:0:120,scale=1920:1080" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn%.*}-hd.mp4" &
 elif [ "$type" == "Square" ]; then
-	exec ffmpeg -i "${fn}" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#090A24,crop=1890:1890:975:255,scale=1080:1080" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn%.*}-square.mp4" &
+	exec ffmpeg -i "${fn}" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#020C14,crop=1890:1890:975:255,scale=1080:1080" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn%.*}-square.mp4" &
 else
-	exec ffmpeg -i "${fn}" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#090A24,crop=3840:2160:0:120" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn%.*}-4k.mp4" &
+	exec ffmpeg -i "${fn}" -vf "fade=t=out:st=${fadeout}:d=${fadeduration}:color=#020C14,crop=3840:2160:0:120" -codec:v libx264 -crf 18 -pix_fmt yuv420p -c:a aac -strict -2 -ss $start -to $end -async 1 -y "${fn%.*}-4k.mp4" &
 fi
 
 echo "Done"
